beb81d91ad088d2934d892e226b63d72a9b01923
Gautam Daga <Gautamdaga62@gmail.com>
2020-10-25 19:11:43 +0530

    Configured postgress db.

--- app/tests/test_models.py
+++ app/tests/test_models.py
@@ -1,8 +1,9 @@
 from django.test import TestCase
 from django.contrib.auth import get_user_model
 
+
 class ModelTests(TestCase):
-    
+
     def test_create_user_with_email_successful(self):
         """Test Creating new user with an email is successful"""
         email = "gautamdaga62@gmail.com"
